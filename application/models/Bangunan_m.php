fd63750fb944a98bb195edf3d26c9cb2f4c2421c
hisarcode <hisarcode@gmail.com>
2020-05-17 09:55:17 +0700

    edit bangunan model

--- application/models/Bangunan_m.php
+++ application/models/Bangunan_m.php
@@ -3,14 +3,47 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Bangunan_m extends CI_Model
 {
+    private $_table = 'bangunan';
+    public $bangunan_id;
+    public $bangunan_nama;
+    public $bangunan_lat;
+    public $bangunan_long;
+    public $bangunan_gambar = "default.jpg";
+    public $bangunan_alamat;
+    public $bangunan_kategori_id;
+    public $kecamatan_id;
+
+    public function rules(){
+        return[
+            ['field' => 'bangunan_nama',
+            'label' => 'Nama Bangunan',
+            'rules' => 'required'],
+
+            ['field' => 'bangunan_lat',
+            'label' => 'Latitude Bangunan',
+            'rules' => 'required'],
+            
+            ['field' => 'bangunan_long',
+            'label' => 'Longitude Bangunan',
+            'rules' => 'required']
+        ];
+    }
     public function getBangunan()
     {
-        return $this->db->get('bangunan')->result_array();
+        $this->db->select('*');
+        $this->db->from('bangunan');
+        $this->db->join('bangunan_kategori', 'kategori_id = bangunan_kategori_id');
+        return $this->db->get()->result_array();
     }
 
     public function getBangunanById($id)
     {
-        return $this->db->get('bangunan', ['bangunan_id' => $id])->row_array();
+        $this->db->select('*');
+        $this->db->from('bangunan');
+        $this->db->join('bangunan_kategori', 'kategori_id = bangunan_kategori_id');
+        $this->db->where('bangunan_id', $id);
+        // return $this->db->get('bangunan', ['bangunan_id' => $id])->row_array();
+        return $this->db->get()->row_array();
     }
 
     public function tambahDataBangunan($data)
@@ -20,10 +53,28 @@ class Bangunan_m extends CI_Model
             'bangunan_id' =>  $this->input->post('id'),
             'bangunan_lat' =>  $this->input->post('lat'),
             'bangunan_long' =>  $this->input->post('long'),
+            'bangunan_gambar' => $this->_uploadImage();
         ];
 
         $this->db->insert('bangunan', $data);
         return $this->db->rowCount();
+    }
+
+    private function _uploadImage($bangunan_id)
+    {
+        $config['upload_path'] = './upload/bangunan/';
+        $config['allowed_types'] = 'jpg|jpeg|png';
+        $config['file_name'] = $bangunan_id;
+        $config['overwrite'] = true;
+        $config['maxsize'] = 1024;
+
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload('image')) {
+            return $this->upload->data("file_name");
+        }
+
+        return "default.jpg";
     }
 
     public function hapusDataBangunan($id)
