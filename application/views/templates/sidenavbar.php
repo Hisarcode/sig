eff10492d5454fbcf4b463c30ffcd75f45c85ba9
hisarcode <hisarcode@gmail.com>
2020-06-14 19:27:06 +0700

    add infographic add login button

--- application/views/templates/sidenavbar.php
+++ application/views/templates/sidenavbar.php
@@ -18,16 +18,15 @@
 					<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 						<!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-						<li class="nav-item has-treeview menu-open">
+						<li class="bangunan nav-item has-treeview menu-open">
 							<a href="<?= base_url('bangunan'); ?>" class="nav-link">
 								<i class="nav-icon fas fa-tachometer-alt"></i>
 								<p>
 									Bangunan
 								</p>
 							</a>
-
-
 						</li>
+
 						<li class="nav-item">
 							<a href="<?= base_url('tentang'); ?>" class="nav-link">
 								<i class="nav-icon fas fa-th"></i>
@@ -36,91 +35,92 @@
 								</p>
 							</a>
 						</li>
+						<li class="nav-item">
+							<a href="<?= base_url('infographic'); ?>" class="nav-link">
+								<i class="nav-icon fas fa-bookmark"></i>
+								<p>
+									Infographic
+								</p>
+							</a>
+						</li>
+						<li class="nav-item">
 
-						<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-							<div class="info">
-								<a href="#" class="d-block">Pilih Kategori</a>
+							<?php if (!($this->session->userdata('username') <> "")) { ?>
+								<a href="<?= base_url('auth'); ?>" class="nav-link">
+									<i class="nav-icon fas fa-user"></i>
+									<p>
+										Login
+									</p>
+								</a>
+							<?php } else { ?>
+								<a href="<?= base_url('auth/logout'); ?>" class="nav-link">
+									<i class="nav-icon fas fa-user"></i>
+									<p>
+										Logout
+									</p>
+								</a>
+							<?php } ?>
+						</li>
+						<?php if ($title == "Eduponmaps") : ?>
+
+							<div class="user-panel mt-3 d-flex">
+								<div class="info">
+									<a href="#" class="d-block">Pilih Kategori</a>
+								</div>
+
 							</div>
 
-						</div>
-						<li class="nav-item has-treeview menu-open">
-							<a href="<?= base_url('bangunan'); ?>" class="nav-link active">
-								<i class="nav-icon fas fa-tachometer-alt"></i>
-								<p>
-									Kecamatan
-								</p>
-							</a>
 
-							<ul class="nav nav-treeview">
-								<li class="nav-item">
-									<a href="pages/examples/login.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Pontianak Tenggara</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Pontianak Timur</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Pontianak Utara</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Pontianak Selatan</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Pontianak Barat</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Pontianak Kota</p>
-									</a>
-								</li>
-							</ul>
-						</li>
-
-						<li class="nav-item has-treeview menu-open">
-							<a href="<?= base_url('bangunan'); ?>" class="nav-link active">
-								<i class="nav-icon fas fa-tachometer-alt"></i>
-								<p>
-									Sekolah
-								</p>
-							</a>
-
-							<ul class="nav nav-treeview">
-								<li class="nav-item">
-									<a href="pages/examples/login.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>SD/MI</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>SMP/MTS</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/examples/register.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>SMA/MA</p>
-									</a>
-								</li>
-							</ul>
-
+							<li class="nav-item has-treeview menu-open">
+								<a class="nav-link active">
+									<i class="nav-icon fas fa-graduation-cap"></i>
+									<p>
+										Sekolah
+									</p>
+								</a>
+							</li>
+						<?php endif; ?>
 					</ul>
+
+					<?php if ($title == "Eduponmaps") : ?>
+						<div class="card-body legenda">
+							<ul class="todo-list ">
+								<li>
+									<div class="icheck-primary d-inline">
+										<input type="checkbox" class="perkategori" name="namaid" id="2" value="2">
+										<label for="2"></label>
+									</div>
+									<span class="text"><img src="<?= base_url('assets/') ?>images/sd.png" width="25" height="25">Fasilitas SD</span>
+
+								</li>
+								<li>
+									<div class="icheck-primary d-inline">
+										<input type="checkbox" class="perkategori" name="namaid" id="3" value="3">
+										<label for="3"></label>
+									</div>
+									<span class="text"><img src="<?= base_url('assets/') ?>images/smp.png" width="25" height="25">Fasilitas SMP</span>
+
+								</li>
+								<li>
+									<div class="icheck-primary d-inline">
+										<input type="checkbox" class="perkategori" name="namaid" id="4" value="4">
+										<label for="4"></label>
+									</div>
+									<span class="text"><img src="<?= base_url('assets/') ?>images/sma.png" width="25" height="25">Fasilitas SMA</span>
+
+								</li>
+								<li>
+									<div class="icheck-primary d-inline">
+										<input type="checkbox" class="perkategori" name="namaid" id="5" value="5">
+										<label for="5"></label>
+									</div>
+									<span class="text"><img src="<?= base_url('assets/') ?>images/lainlain.png" width="25" height="25">Fasilitas SMK</span>
+
+								</li>
+							</ul>
+						</div>
+					<?php endif; ?>
+					<!-- AKHIR PANEL LEGENDA-->
 				</nav>
 				<!-- /.sidebar-menu -->
 			</div>
