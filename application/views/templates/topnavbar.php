3ce664c807273afbc1796e7a614fe4430d1491e4
hisarcode <hisarcode@gmail.com>
2020-06-14 19:27:32 +0700

    add login button and infographic

--- application/views/templates/topnavbar.php
+++ application/views/templates/topnavbar.php
@@ -8,7 +8,7 @@
             </li>
 
             <li class="nav-item d-sm-inline-block">
-                <a class="nav-link text-dark">Education Pontianak Maps</a>
+                <a href="<?= base_url() ?>" class="nav-link text-dark"><b>Edupon</b>maps</a>
             </li>
 
             <li class="nav-item d-none d-sm-inline-block">
@@ -18,7 +18,36 @@
             <li class="nav-item d-none d-sm-inline-block">
                 <a href="<?= base_url('tentang/') ?>" class="nav-link">Tentang</a>
             </li>
+
+
+            <?php if ($title == "Eduponmaps") :  ?>
+                <!-- SEARCH FORM -->
+                <form class="form-inline ml-3" role="search">
+                    <div class="input-group input-group-sm">
+                        <input class="form-control form-control-navbar" type="text" id="keyword" autocomplete="off" placeholder="Search" aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-navbar" type="button" id="caribykeyword">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            <?php endif; ?>
+
+
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="<?= base_url('infographic') ?>" class="nav-link">Infographic</a>
+            </li>
         </ul>
 
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <?php if (!($this->session->userdata('username') <> "")) { ?>
+                    <a href="<?= base_url('auth') ?>" class="nav-link text-dark">Login</a>
+                <?php } else { ?>
+                    <a href="<?= base_url('auth\logout') ?>" class="nav-link text-dark">Logout</a>
+                <?php } ?>
+            </li>
+        </ul>
     </nav>
     <!-- /.navbar -->
